3617d412ab6f7255c0609adee886c1ba8bebd55d
Ashley Tagge <ashley70@byu.edu>
2025-02-27 00:31:44 -0700

    replace hardcoding with local variable/api placeholder

--- src/treasure/treasure.jsx
+++ src/treasure/treasure.jsx
@@ -3,16 +3,36 @@ import { useNavigate } from 'react-router-dom';
 import './treasure.css';
 
 export function Treasure() {
+  //local storages
   const navigate = useNavigate();
+  const [image, setImage] = useState('mermaid.png'); // Placeholder image before api image generator
+  const newpoints = JSON.parse(localStorage.getItem('newpoints')) || 100; //get newpoints and use 100 as a saftey net
+
+  useEffect(() => {
+    // create my api random image generator placeholder
+    const randomImage = 'mermaid.png'; 
+
+    // Get existing images list or initialize empty array to store images to display on treasure chest
+    const images = JSON.parse(localStorage.getItem('images')) || [];
+
+    // Add the new marine image to the front of the list
+    images.unshift(randomImage);
+
+    // Save the updated list to localStorage
+    localStorage.setItem('images', JSON.stringify(images));
+
+    // Set current image
+    setImage(randomImage);
+  }, []);
 
   return (
     
     <main>
   <div className="congrats-container">
     <div className="congrats"><h2>Congratulations!</h2></div>
-    <div className="points"><p>+150 POINTS</p></div>
+    <div className="points"><p>+{newpoints} POINTS</p></div>
     <div className="image-container">
-      <img src="mermaid.png" alt="mermaid" width="200" />
+      <img src={image} alt="marine animal" width="200" />
     </div>
     <div className="buttons-container">
       <button onClick={() => navigate('/treasurechest')}>Open Treasure Chest</button>
